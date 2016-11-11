e9cecb484b3cc77f8961cef1d21f93a6c1d84232
KyleJestery <kyl3jester@gmail.com>
2016-11-10 19:41:16 -0500

    CSS Cimpilation Update
    Git errors with merge.  cloned and readded

--- Owlendar/www/js/app.js
+++ Owlendar/www/js/app.js
@@ -35,6 +35,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.calendar'])
     controller: 'AppCtrl'
   })
 
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
   .state('app.calendar', {
       url: '/calendar',
       views: {
@@ -42,7 +51,26 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ui.calendar'])
           templateUrl: 'templates/calendar.html'
         }
       }
-    });
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/calendar');
 });
